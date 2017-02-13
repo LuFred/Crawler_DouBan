7637961b6146c422e5baea5704743dd3e6bf92bd
home\fred.lu <fred@vidahouse.com>
2017-02-13 16:25:44 +0800

    test

---
+++ Crawler.Host/Components/Kafka/ProduceHelper.cs
@@ -0,0 +1,32 @@
+﻿using RdKafka;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crawler.Host.Components.Kafka
+{
+    public class ProduceHelper
+    {
+        private Producer _produce;
+        public ProduceHelper(string conn)
+        {   
+            _produce = new Producer(conn);
+        }
+        public void CreateTopic(string topicName)
+        {
+          //  _produce.Topic(topic)
+        }
+        public void PushMessage(string topicName,string message) {
+            using (Topic topic=_produce.Topic(topicName))
+            {
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                DeliveryReport deliveryReport = topic.Produce(data).Result;
+                Console.WriteLine($"Produced to Partition: {deliveryReport.Partition}, Offset: {deliveryReport.Offset}");
+            }
+
+        }
+        }
+    
+}
--- Crawler.Host/Program.cs
+++ Crawler.Host/Program.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Net.Http;
 using System.Collections.Generic;
 using Crawler.Core.Model;
+using Crawler.Host.Components.Kafka;
 
 namespace Crawler.Host
 {
@@ -14,7 +15,7 @@ namespace Crawler.Host
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
 
-           
+
 
             ttt();
             Console.ReadLine();
@@ -22,24 +23,29 @@ namespace Crawler.Host
 
         public static void ttt()
         {
-           
-            var host = new Client();
-            //var r=host.SearchSubject("热门", 0, 20).Result;
-        List<MovieTagModel> movieTagModelList= host.GetDouBanMovieAllTags("https://movie.douban.com/tag/");
-        foreach(var t in movieTagModelList){
-            Console.WriteLine(t.TagName);
-        }
-        for(int i=0;i<movieTagModelList.Count;i++){
-            var nextPageUrl=movieTagModelList[i].Url;
-            List<MovieInfoModel> infoList=new  List<MovieInfoModel>();
-            while(nextPageUrl!=null&&nextPageUrl.Length>0){
-                var list= host.GetMovieInfo(nextPageUrl,out nextPageUrl);
-                infoList.AddRange(list);
-            }
-            
-        }
+            Console.WriteLine("begin create topic");
+            ProduceHelper produceHelper = new ProduceHelper("http://nosrc.com:2181");
+            produceHelper.PushMessage("科幻", "nihaoma ");
+            //var host = new Client();
+            //List<MovieTagModel> movieTagModelList = host.GetDouBanMovieAllTags("https://movie.douban.com/tag/");
+            //foreach (var t in movieTagModelList)
+            //{
+            //    Console.WriteLine(t.TagName);
+            //}
+
+            //for (int i = 0; i < movieTagModelList.Count; i++)
+            //{
+            //    var nextPageUrl = movieTagModelList[i].Url;
+            //    List<MovieInfoModel> infoList = new List<MovieInfoModel>();
+            //    while (nextPageUrl != null && nextPageUrl.Length > 0)
+            //    {
+            //        var list = host.GetMovieInfo(nextPageUrl, out nextPageUrl);
+            //        infoList.AddRange(list);
+            //    }
+
+            //}
         }
 
-       
+
     }
 }
